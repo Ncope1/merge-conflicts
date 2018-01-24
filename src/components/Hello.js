91203d05657535f44ad39867729c54b262e38afe
Your Name <you@example.com>
2018-01-24 11:08:45 -0500

    made hello component

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Hello extends Component {
+    render() {
+        let name = 'Marquet & Natalee'
+        return(
+            <div>
+             <p>Hello {name}</p>   
+            </div>
+        )
+    }
+}
+
+export default Hello
